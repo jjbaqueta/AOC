4bebceb0d8dda08b2ee040dadfe46d3645037173
Jeferson José Baqueta <42413563+jjbaqueta@users.noreply.github.com>
2024-07-26 15:13:03 -0300

    Add files via upload

---
+++ aula11exer2_b.asm
@@ -0,0 +1,13 @@
+.data
+	var1: .space 1
+	.align 2		# alinhando o endereço da próxima variável com um endereço múltiplo de 4 (2^2)
+	var2: .space 4
+	var3: .space 2
+.text
+	li $t0, 5
+	sb $t0, var1
+	sw $t0, var2
+	sh $t0, var3
+
+	li $v0, 10
+	syscall 
