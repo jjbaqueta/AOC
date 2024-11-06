0ac4789110b0ea33121f36cc256df90f8a792921
Jeferson José Baqueta <42413563+jjbaqueta@users.noreply.github.com>
2024-11-06 13:48:00 -0300

    Delete aula11exer1_b.asm

--- aula11exer1_b.asm
+++
@@ -1,9 +0,0 @@
-.data
-	a: .word 10
-	result: .space 4
-.text
-	lw $t0, a		# carrega o valor da variável 'a' da memória para $t0
-	addi $t0, $t0, -1	# decrementa o valor armazenado em $t0 em uma unidade
-	sw $t0, result		# escreve o valor de $t0 na variável 'result'
-	li $v0, 10		# finalização do programa
-	syscall
