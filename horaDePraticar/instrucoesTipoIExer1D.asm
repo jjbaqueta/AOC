2b673e4588541734a79f30dab12b46c6a8ccca7a
Jeferson José Baqueta <42413563+jjbaqueta@users.noreply.github.com>
2024-11-06 12:25:50 -0300

    Update instrucoesTipoIExer1D.asm

--- horaDePraticar/instrucoesTipoIExer1D.asm
+++ horaDePraticar/instrucoesTipoIExer1D.asm
@@ -15,8 +15,8 @@
 
 .text
   lh $t0, a           # Carrega o valor da variável 'a' da memória para $t0
-  lh $t1, b		        # Carrega o valor da variável 'b' da memória para $t1
-  add $t0, $t0, $t1	  # Soma o valor de $t0 com o valor de $t1, o resultado é armazenado em $t0
-  sh $t0, result		  # Escreve o valor de $t0 na variável 'result'
-  li $v0, 10		      # Finalização do programa
+  lh $t1, b           # Carrega o valor da variável 'b' da memória para $t1
+  add $t0, $t0, $t1   # Soma o valor de $t0 com o valor de $t1, o resultado é armazenado em $t0
+  sh $t0, result      # Escreve o valor de $t0 na variável 'result'
+  li $v0, 10          # Finalização do programa
   syscall
