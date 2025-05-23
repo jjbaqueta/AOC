d324b2d80244af78fcd1b5425edfbc1e5ad04b45
Jeferson José Baqueta <42413563+jjbaqueta@users.noreply.github.com>
2025-05-23 18:41:48 -0300

    Update funcaoParidade.asm

--- exemplos/funcaoParidade.asm
+++ exemplos/funcaoParidade.asm
@@ -1,8 +1,8 @@
 # Exercício
-# Implemente uma função chamada ePar que recebe, por parâmetro, um número inteiro. 
-# Essa função deve retornar 1 se o número informado for par, ou 0 caso seja ímpar. 
+# Implemente uma função chamada eImpar que recebe, por parâmetro, um número inteiro. 
+# Essa função deve retornar 1 se o número informado for ímpar, ou 0 caso seja par. 
 # O número passado por parâmetro deve ser lido de uma variável chamada num (com 4 bytes de tamanho). 
-# Utilize apenas registradores do tipo $s em seu programa.
+# Utilize apenas registradores do tipo S em seu programa.
 #
 # Exemplo de entrada:
 # -  Entrada 1: num = 10
@@ -23,7 +23,8 @@
    # Função epar: verifica se um número é par
    # Recebe um número inteiro por parâmetro ($a0)
    # Retorna o resto da divisão do número por 2 ($v0)
-   ePar:
+   
+   eImpar:
    addi $sp, $sp, -4     # Aloca espaço na pilha para salvar $s0
    sw $s0, ($sp)         # Salva o valor de $s0 na pilha
    li $s0, 2             # Carrega o valor 2 em $s0 (divisor)
@@ -37,7 +38,7 @@
    main:
    lw $s0, num           # Carrega o valor da variável 'num' em $s0
    move $a0, $s0         # Move o valor de $s0 para $a0 (argumento para função 'epar')
-   jal ePar              # Chama a função 'epar'
+   jal eImpar            # Chama a função 'eImpar'
    move $s1, $v0         # Armazena o retorno da função em $s1   
 
    move $a0, $s0         # Move o número de entrada (em $s0) para $a0 (para impressão)
@@ -52,6 +53,5 @@
    FIM:
    li $v0, 4             # Código de impressão de string
    syscall               # Imprime a string correspondente
-   
    li $v0, 10            # Código para encerrar o programa
    syscall               # Termina o programa
