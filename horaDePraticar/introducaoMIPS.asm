ed15996b7f31957a8254196c17fa15ffd04dc90d
Jeferson José Baqueta <42413563+jjbaqueta@users.noreply.github.com>
2025-09-17 13:34:28 -0300

    Update introducaoMIPS.asm

--- horaDePraticar/introducaoMIPS.asm
+++ horaDePraticar/introducaoMIPS.asm
@@ -1,29 +1,34 @@
 # Exercício
 # Realize as seguintes operações:
-# - Mova o valor 15 para o registrador $t5
-# - Mova o valor 64 para o registrador $s0
-# - Exiba o valor 34 na saída do simulador
-# - Exiba o valor 50 na saída do simulador
-#
-# Saída esperada (aba Run I/O):
-# 3450
+# 1) Mova o valor 15 para o registrador $t5
+# 2) Mova o valor 64 para o registrador $s0
+# 3) Exiba o valor 34 no terminal do simulador
+# 4) Exiba o valor 50 no terminal do simulador
+# 5) Leia um número inteiro informado pelo usuário e exiba-o no terminal do simulador
 
 .data
 .text
 
-  # Move o valor 15 para o registrador $t5
+  # 1) Move o valor 15 para o registrador $t5
   li $t5, 15
   
-  # Move o valor 64 para o registrador $s0
+  # 2) Move o valor 64 para o registrador $s0
   li $s0, 64
   
-  # Exibe o valor 34 na saída do simulador
+  # 3) Exibe o valor 34 no terminal
   li $a0, 34
   li $v0, 1
   syscall
   
-  # Exibe o valor 50 na saída do simulador
+  # 4) Exibe o valor 50 no terminal
   li $a0, 50
+  li $v0, 1
+  syscall
+
+  # 5) Exibe um número inteiro informado pelo usuário
+  li $v0, 5
+  syscall
+  move $a0, $v0
   li $v0, 1
   syscall
   
